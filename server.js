438f40def8bd21f8d5a5507022d71be105ff7bf3
Meon <test@test.test>
2016-06-30 17:45:45 +1000

    add res.redirect to server.js

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+//OpenWeatherMap middleware to redirect all traffic to http: not https:
+
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
